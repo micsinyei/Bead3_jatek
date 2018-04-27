d75d243e4fd14fd575defc893732f86d4bbc6b8d
Laci <lmicsinyei@gmail.com>
2018-04-27 19:19:42 +0200

    Widget feltoltese

---
+++ widgets.cpp
@@ -0,0 +1,17 @@
+#include "widgets.hpp"
+#include "graphics.hpp"
+
+using namespace genv;
+
+Widget::Widget(int kx, int ky, int w, int h) {
+  x = kx;
+  y = ky;
+  width = w;
+  height = h;
+  focus=false;
+}
+
+bool Widget::Selected(int mouse_x, int mouse_y) {
+    return mouse_x>x && mouse_x<x+width && mouse_y>y && mouse_y<y+height;
+}
+
---
+++ widgets.hpp
@@ -0,0 +1,19 @@
+#ifndef WIDGETS_HPP_INCLUDED
+#define WIDGETS_HPP_INCLUDED
+
+#include "graphics.hpp"
+
+class Widget {
+protected:
+    int x, y, width, height;
+    bool focus;
+public:
+    Widget(int kx, int ky, int w, int h);
+    virtual bool Selected(int mouse_x, int mouse_y);
+    virtual void Draw() const = 0;
+    virtual void Handle(genv::event ev) =  0;
+
+};
+
+
+#endif
