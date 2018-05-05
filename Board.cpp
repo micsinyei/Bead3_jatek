601a0442d673eba6ffea2530df1010ecab455e1a
Laci <lmicsinyei@gmail.com>
2018-05-05 19:04:29 +0200

    Újra lett írva ellenorzi hogy helyes a lépés verticális és horizontális ellenörzés is van

--- Board.cpp
+++ Board.cpp
@@ -3,45 +3,51 @@
 #include <iostream>
 #include "Circle.hpp"
 using namespace genv;
-Board::Board(int x, int y, int w, int h):Widget(x,y,w,h){
-    for(int i=0;i<7;i++){
-        for(int j=0;j<6;j++){
-            b[i][j]=0;
+Board::Board(int kx, int ky, int w, int h):Widget(kx,ky,w,h) {
+    std::cout<<"test";
+    for(int i=0; i<COLUMNS; i++) {
+        for(int j=0; j<ROWS; j++) {
+            b[i][j]=3;
         }
     }
     boxsize=70;
     circlesize=boxsize/2-5;
+    width=x+7*boxsize;
     //b[0][0]=1;
     //b[6][5]=2;
-
+    Draw();
+    std::cout<<"test";
 }
-void Board::Handle(genv::event ev){
-  for(int i=0;i<7;i++){
-  gout<<move_to(x,y-boxsize)<<color(0,0,0)<<box(width,boxsize);
-        if(x+(boxsize*i)<ev.pos_x&&ev.pos_x< x+boxsize+(boxsize*i)){
-            gout<<move_to(x+(boxsize*i),y-boxsize)<<color(0,0,0)<<box(boxsize,boxsize);
-            gout <<color(255,255,255);
-            Circle(x+boxsize/2+(boxsize*i),y-boxsize+boxsize/2,circlesize);
+void Board::Handle(genv::event ev) {
+    /*
+      for(int i=0;i<7;i++){
+
+            if(x+(boxsize*i)<ev.pos_x&&ev.pos_x< x+boxsize+(boxsize*i)){
+            gout<<move_to(x,y-boxsize)<<color(0,0,0)<<box(width,boxsize);
+            std::cout<<width+x<<" "<<ev.pos_x<<std::endl;
+                gout<<move_to(x+(boxsize*i),y-boxsize)<<color(0,0,0)<<box(boxsize,boxsize);
+                gout <<color(255,255,255);
+                Circle(x+boxsize/2+(boxsize*i),y-boxsize+boxsize/2,circlesize);
+            }
         }
-    }
-    // mouse_x>x && mouse_x<x+width && mouse_y>y && mouse_y<y+height;
-    //ev.pos_x, ev.pos_y
+        // mouse_x>x && mouse_x<x+width && mouse_y>y && mouse_y<y+height;
+        //ev.pos_x, ev.pos_y*/
 }
 void Board::Draw() const {
-
-gout<<color(255,255,255);
-for(int i=0;i<7;i++){
-        for(int j=0;j<6;j++){
-            if( b[i][j]==0){
+    std::cout<<"test";
+    gout<<color(255,255,255);
+    for(int i=0; i<7; i++) {
+        for(int j=0; j<6; j++) {
+            if( b[i][j]==3) {
                 gout<<move_to(x+(boxsize*i),y+(boxsize*j))<<color(0,0,204)<<box(boxsize,boxsize);
                 gout <<color(255,255,255);
                 Circle(x+boxsize/2+(boxsize*i),y+boxsize/2+(boxsize*j),circlesize);
-            }else if( b[i][j]==1){
+            } else if( b[i][j]==1) {
                 gout<<move_to(x+(boxsize*i),y+(boxsize*j))<<color(0,0,204)<<box(boxsize,boxsize);
                 gout <<color(228,0,0);
                 Circle(x+boxsize/2+(boxsize*i),y+boxsize/2+(boxsize*j),circlesize);
-            } else if( b[i][j]==2){
-                 gout<<move_to(x+(boxsize*i),y+(boxsize*j))<<color(0,0,204)<<box(boxsize,boxsize);
+            } else if( b[i][j]==0) {
+                gout<<move_to(x+(boxsize*i),y+(boxsize*j))<<color(0,0,204)<<box(boxsize,boxsize);
                 gout <<color(255,255,0);
                 Circle(x+boxsize/2+(boxsize*i),y+boxsize/2+(boxsize*j),circlesize);
             }
@@ -51,6 +57,45 @@ for(int i=0;i<7;i++){
     }
 
 }
-void Board::SetBoard(int col,int row,int v){
-    b[col][row]=v;
+void Board::addGamePiece(const int col, const int gamePiece) {
+    int ind=ROWS-1;
+    while(b[col][ind]!=3) {
+        ind--;
+    }
+    if(ind<0) {
+        std::cout<<"egg are not supposed to be green"<<std::endl;
+    } else {
+        b[col][ind]=gamePiece;
+    }
+}
+bool Board::CheckWinner(int turn) {
+///vertical
+    for(int i=0; i<COLUMNS-4; i++) {
+        for(int j=0; j<ROWS; j++) {
+            if(b[i][j]==turn) {
+                if(b[i+1][j]==turn) {
+                    if(b[i+2][j]==turn) {
+                        if(b[i+3][j]==turn) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    ///horizontal
+     for(int i=0; i<COLUMNS; i++) {
+        for(int j=0; j<ROWS-3; j++) {
+            if(b[i][j]==turn) {
+                if(b[i][j+1]==turn) {
+                    if(b[i][j+2]==turn) {
+                        if(b[i][j+3]==turn) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
 }
--- Board.hpp
+++ Board.hpp
@@ -2,15 +2,23 @@
 #define BOARD_HPP_INCLUDED
 #include "widgets.hpp"
 
-class Board: public  Widget{
+class Board: public  Widget {
 protected:
     int b[7][6];
     int boxsize,circlesize;
- public:
- Board(int x, int y, int w, int h);
-  virtual void Draw() const ;
-  virtual void Handle(genv::event ev) ;
-  virtual void SetBoard(int col,int row,int value);
+public:
+    Board(int kx, int ky, int w, int h);
+    virtual void Draw() const ;
+    virtual void Handle(genv::event ev) ;
+    static const int ROWS = 6;
+    static const int COLUMNS = 7;
+;
+
+    int getPosition(int row, int col) const {
+        return b[row][col];
+    }
+    void addGamePiece( const int col, const int gamePiece);
+    bool CheckWinner(int turn);
 
 };
 
